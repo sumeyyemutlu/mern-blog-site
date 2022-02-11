40f16e025f8c5d8c93431cb5ddbd8bd6d37d5d31
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-02-11 18:02:50 +0300

    a new action added

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,8 +1,29 @@
+import * as api from "../api/index"
 import * as types from "./types"
 
-export const fetchPosts = () => {//burası actionCreator, action oluşturduk.
-    return {
-        type: types.FETCH_POSTS,
-        payload: [] //veritabanından alacağız
+//redux-thunk kullandık. fonk tanımladık çünkü
+export const fetchPosts = () => async(dispatch) => {//burası actionCreator, action oluşturduk.
+    try {//burada asenkron bir işlem yapmak istediğim için redux-thunk yapısını kullandık
+        const { data } = await api.fetchPosts();
+        dispatch({
+            type: types.FETCH_POSTS,
+            payload: data //veritabanından alacağız
+        })
+        
+    } catch (error) {
+        console.log(error)
     }
+}
+
+export const createPost = (post) => async (dispatch) => {//iki parametre aldı
+    try {
+        const {data} = await api.createPost(post);//addPostFormdan gelen post verisini al api içindeki createPost a gönder dedik
+        dispatch({
+            type: types.CREATE_POST,
+            payload: data,
+        })
+    } catch (error) {
+        console.log(error)
+    }
+   
 }
