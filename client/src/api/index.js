e31e33cf48268d48350a377a9e87dfe6106f621b
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-02-11 17:58:38 +0300

    api folder created

---
+++ client/src/api/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+const apiEndpoint = "http://localhost:5000/posts/" //apinin posts kısmına istek yapıldığında
+
+export const fetchPosts = async () => await axios.get(apiEndpoint)
+//controller içindeki getPost ile dönen json verisini burada  fetchPost ile alıyoruz
+//veritabanıa erişim sağlanacağından dolayı bekleme olmasın diye asenkron tanımladık.
+
+
+export const createPost  = async (post) => await axios.post(apiEndpoint, post)//axios.post bir post işlemi yapılacak demek backenddeki server a (buradan sonra backend kısmındaki routes ve controllers a geçiyoruz)
+//axios.post içindeki post ise bizim addPostFormdan gönderdiğimiz post verisi
+//actionların içindeki createPost actionu içinden buraya bağlantı sağladık
+//bunu da controller içindeki createPosttın içinden req.bodyden yeni bir post oluşturuyor
+//sonra da veri tabanına kaydediyor
