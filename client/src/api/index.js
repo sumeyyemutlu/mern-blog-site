6fe22dbc31b54464656893fff07e3fd765281aff
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-02-13 19:18:00 +0300

    fetchSinglePost api created

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,6 +6,10 @@ export const fetchPosts = async () => await axios.get(apiEndpoint)
 //veritabanıa erişim sağlanacağından dolayı bekleme olmasın diye asenkron tanımladık.
 
 
+export const fetchSinglePost = async (id) => await axios.get(`${apiEndpoint}{id}`)
+//fetchSinglePost actionundan gelen id bilgisini burada apiEndPoint'in sonuna ekledik
+//şimdi server tarafına geçeceğiz, routes ve controller tanımlanacak
+
 export const createPost  = async (post) => await axios.post(apiEndpoint, post)//axios.post bir post işlemi yapılacak demek backenddeki server a (buradan sonra backend kısmındaki routes ve controllers a geçiyoruz)
 //axios.post içindeki post ise bizim addPostFormdan gönderdiğimiz post verisi
 //actionların içindeki createPost actionu içinden buraya bağlantı sağladık
