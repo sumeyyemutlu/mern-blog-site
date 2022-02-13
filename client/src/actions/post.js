8a9a8f6c1a5f9138d4fae34a1c46c09d4e8caacc
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-02-13 19:20:09 +0300

    fetchSinglePost action created

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -14,8 +14,20 @@ export const fetchPosts = () => async(dispatch) => {//burası actionCreator, act
         console.log(error)
     }
 }
+export const fetchSinglePost = (id) => async (dispatch) => {//iki parametre aldı.
+    try {
+        const {data} = await api.fetchSinglePost(id);//PostDetails ten gelen id verisini al api içindeki fetchSinglePost a gönder dedik
+        dispatch({
+            type: types.FETCH_SINGLE_POST,
+            payload: data,
+        })
+    } catch (error) {
+        console.log(error)
+    }
+   
+}
 
-export const createPost = (post) => async (dispatch) => {//iki parametre aldı
+export const createPost = (post) => async (dispatch) => {//iki parametre aldı.
     try {
         const {data} = await api.createPost(post);//addPostFormdan gelen post verisini al api içindeki createPost a gönder dedik
         dispatch({
