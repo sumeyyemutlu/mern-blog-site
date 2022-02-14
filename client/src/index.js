852dd3c2da18aca3755a7001003ebe1bb944b44a
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-02-14 21:52:02 +0300

    alertify added

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./store"
+import "alertifyjs/build/css/alertify.min.css"
 
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  
     <Provider store={store}><App /></Provider>,
    
-  </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
